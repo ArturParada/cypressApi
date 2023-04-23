4a62cf6c8c5a14400129cd8fc7fb856bad832f24
Artur <paradaartur@gmail.com>
2023-04-23 14:53:01 +0200

    validate get request

--- cypress/e2e/get-request.js
+++ cypress/e2e/get-request.js
@@ -6,4 +6,19 @@ describe('"Get Request', () => {
         result = cy.request("http://localhost:3000/posts")
         result.its("status").should("equal", 200)
     });
+    it('Validate /posts api contains the correct keys and values', () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/posts",
+            headers: {
+                accept: "application/json"
+            }
+        }).then((response) => {
+            let body = JSON.parse(JSON.stringify(response.body))
+            cy.log(body)
+
+            expect(body[0]).has.property("title", "Example Json Server")
+            expect(body[0]).has.property("author", "Arczi")
+        })
+    });
 });
